7552727bb2eaf41cf8f3af98828e64a5361296b0
Ed Spencer <ed@edspencer.net>
2024-07-10 11:30:14 -0400

    remove markdoc

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import withMarkdoc from '@markdoc/next.js'
-
 import withSearch from './src/markdoc/search.mjs'
 
 /** @type {import('next').NextConfig} */
@@ -7,6 +5,4 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
 }
 
-export default withSearch(
-  withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig),
-)
+export default withSearch(nextConfig)
