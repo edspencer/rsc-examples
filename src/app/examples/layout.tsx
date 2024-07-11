a32497042c1597a084776be425eb1cd9ce068ef1
Ed Spencer <ed@edspencer.net>
2024-07-11 08:05:12 -0400

    iframe refresh

--- src/app/examples/layout.tsx
+++ src/app/examples/layout.tsx
@@ -1,3 +1,5 @@
+import { headers } from 'next/headers'
+
 export const metadata = {
   title: 'RSC Examples Gallery by Ed Spencer',
   description: 'A set of React Server Components examples by Ed Spencer',
@@ -25,6 +27,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  //we don't actually care about headers, this is just to get Next JS to treat all of our pages as
+  //dynamic. Otherwise, when we create a production build all of our pages will be statically generated
+  //and the demo won't show anything as it will all just load instantly as static HTML.
+  const pageHeaders = headers()
+
   return (
     <html lang="en">
       <body className={`${prose} p-4`}>{children}</body>
--- src/components/FullPageDemo.tsx
+++ src/components/FullPageDemo.tsx
@@ -1,5 +1,9 @@
+'use client'
+
+import { useRef } from 'react'
 import Link from 'next/link'
 import VideoPreview from '@/components/VideoPreview'
+import { Button } from '@/components/Button'
 
 export function FullPageDemo({
   slug,
@@ -8,6 +12,13 @@ export function FullPageDemo({
   slug?: string
   fullPage: boolean
 }) {
+  const iframeRef = useRef<HTMLIFrameElement>(null)
+  const handleRefresh = () => {
+    if (iframeRef.current) {
+      iframeRef.current.src = iframeRef.current.src
+    }
+  }
+
   if (!fullPage) return null
 
   return (
@@ -18,12 +29,31 @@ export function FullPageDemo({
         feels to the end user. Here it is inside an iframe, and there's a
         looping video below too.
       </p>
-      <p>
-        <Link target="_blank" href={`/examples/${slug}`}>
-          Click here to test it out yourself
-        </Link>
-      </p>
+      <div className="flex items-center justify-between">
+        <p>
+          <Link target="_blank" href={`/examples/${slug}`}>
+            Click here to test it out yourself
+          </Link>
+        </p>
+        <Button variant="blank" onClick={handleRefresh} title="Refresh">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+            />
+          </svg>
+        </Button>
+      </div>
       <iframe
+        ref={iframeRef}
         src={`/examples/${slug}`}
         className="h-96 w-full rounded-md border border-slate-500"
       />
